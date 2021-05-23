65c5c8596fa2c1068335780d50b0dbd791e24c52
Jake Wang <wang7367@umn.edu>
2021-05-22 23:06:42 -0500

    Update solution.js

--- solution.js
+++ solution.js
@@ -1,10 +1,9 @@
 {        
-    //Passing rate: 50%, failure cases mostly need to transport one more person to pass
-    
-    //The basic idea of this solution is to let the elevator go back and forth between the top floor and the groud floor,
-	//and take care of requests by floor sequence
-	//If the elevator is going up, then take requests and arrange them in ascending order 
-    //If the elevator is going down, then take requests and arrange them in descending order 
+    //Passing rate: 95%
+
+    //Same idea as the solution for challenge #2
+	//but change the direction of the elevator once we find out there is no more requests on its way of the current direction
+	//instead of only change the direction when it reaches the top floor or the groud floor, to reduce the waiting time
     init: function(elevators, floors) {
         var elevator = elevators[0]; 
 
@@ -25,42 +24,32 @@
                 if(nextRoundDown.length!=0){
                     elevator.goToFloor(nextRoundDown[0]);
                 }else{
-                    up=!up;
-                    elevator.goingUpIndicator(up);
-                    elevator.goingDownIndicator(!up);						
-                    elevator.goToFloor(0);
-                }    
+                        up=!up;
+                        elevator.goToFloor(0);
+                } 
             }
         });
-
-
+        
         //After a passenger presses a button inside the elevator
         //Add that floor number to the queue 
         //and sort the queue in ascending order if the elevator is going up
         //sort the queue in descending order if the elevator is going down
         elevator.on("floor_button_pressed", function(floorNum){
-            if(!elevator.destinationQueue.includes(floorNum)){
+            if(!elevator.destinationQueue.includes(floorNum)) {
                 elevator.goToFloor(floorNum,true);        
-                if(up) sortInAscending();
+                if(up) sortInAscending();                  
                 else sortInDescending();
             }
         });
 
-
-        //After the elevator is done transporting all the passengers, 
-		//if the elevator has reached the top floor or the ground floor
-        //change the direction and swap the indicators of the elevator.
+        //After the elevator is done transporting all the passengers,
+        //instead of must go to the top floor or the groud floor and then change the direction,
+        //check if there is no one needs to be picked up beyond the floor the elevator currently is on
+        //if yes, then change the direction right away
         elevator.on("stopped_at_floor", function(floorNum){
-
             if(doneTrans()){
-                if((elevator.currentFloor()==topFloor)||(elevator.currentFloor()==0)){
-                    up=!up;
-                    elevator.goingUpIndicator(up);
-                    elevator.goingDownIndicator(!up);
-                    merge();
-                }          
-                if(up) elevator.goToFloor(topFloor); //make sure the elevator gets to the top or ground
-                else  elevator.goToFloor(0);
+                if(up) turnDown(floorNum);
+                else   turnUp(floorNum);
             } 
         });
 
@@ -73,11 +62,11 @@
                 //check if the passenger can be picked up on its way
                 //If can't, we leave that floor to the next round when the elevator goes up again
                 if(canBePickedUp(floorNum, "up")){ 
-                            //make sure the elevator was not planning to that floor
-                            if(!elevator.destinationQueue.includes(floorNum)){
-                                elevator.goToFloor(floorNum,true);  
-                                sortInAscending();
-                            }
+                    //make sure the elevator was not planning to that floor
+                    if(!elevator.destinationQueue.includes(floorNum)){
+                        elevator.goToFloor(floorNum,true);  
+                        sortInAscending();
+                    }
                 }else{
                     if(!isQueued(up,floorNum)){
                         nextRoundUp.push(floorNum);
@@ -92,11 +81,11 @@
                 //check if the passenger can be picked up on its way
                 //If can't, we leave that floor to the next round when the elevator goes down again
                 if(canBePickedUp(floorNum, "down")){ 
-                            //make sure the elevator was not going to that floor
-                            if(!elevator.destinationQueue.includes(floorNum)){
-                                elevator.goToFloor(floorNum,true);
-                                sortInDescending();
-                            }
+                    //make sure the elevator was not going to that floor
+                    if(!elevator.destinationQueue.includes(floorNum)){
+                        elevator.goToFloor(floorNum,true);
+                        sortInDescending();
+                    }
                 }else{
                     if(!isQueued(up,floorNum)){
                         nextRoundDown.push(floorNum);
@@ -145,6 +134,34 @@
             }
         }
 
+        //change the direction of the elevator to down if it meets all the conditions
+        //or goes to the floor where a passenger needs to be pick up beyond its current floor
+        function turnDown(floorNum){
+            //if no one requests beyond its current floor on the opposite direction or it has reached to the top floor
+            if(nextRoundDown.length==0||nextRoundDown[0]<=floorNum||floorNum==topFloor){
+                up=!up;
+                elevator.goingUpIndicator(up);
+                elevator.goingDownIndicator(!up);
+                merge();
+            }else{
+                elevator.goToFloor(nextRoundDown[0]);
+            }   
+        }
+        
+        //change the direction of the elevator to up if it meets one of the conditions
+        //or goes to the floor where a passenger needs to be pick up beyond its current floor
+        function turnUp(floorNum){
+            //if no one requests beyond its current floor on the opposite direction or it has reached to the groud floor
+            if(nextRoundUp.length==0||floorNum<=nextRoundUp[0]||floorNum==0){
+                up=!up;
+                elevator.goingUpIndicator(up);
+                elevator.goingDownIndicator(!up);
+                merge();
+            }else{
+                elevator.goToFloor(nextRoundUp[0]);
+            }  
+        }
+
         //sort the queue in ascending order
         function sortInAscending(){
             elevator.destinationQueue.sort(function(a, b){return a - b});  
@@ -161,23 +178,25 @@
         //yes if the elevator is going to the same direction as where the passenger is at
         //and is not passed yet and not full
         function canBePickedUp(floorNum, dir){
-          if(dir==="up"){
-             if(up && !isPassed(floorNum) && isNotFull()){
-                 return true;
-             }else{
-                 return false;
-             }
-          }   
-            
-          if(dir==="down"){ 
-            if(!up && !isPassed(floorNum) && isNotFull()){
-                return true;
-            }else{
-                return false;
+            if(dir==="up"){
+                if(up && !isPassed(floorNum) && isNotFull()){
+                    return true;
+                }else{
+                    return false;
+                }
+            }   
+
+            if(dir==="down"){ 
+                if(!up && !isPassed(floorNum) && isNotFull()){
+                    return true;
+                }else{
+                    return false;
+                }
             }
-          }
         }
-        
+         
+
+
     },
         update: function(dt, elevators, floors) {
             // We normally don't need to do anything here
